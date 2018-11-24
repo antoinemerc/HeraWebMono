1e1bdc778d5781ad98573f42f22bf1c221f52b90
Juanpii <bidalia1100@hotmail.com>
2018-11-24 18:37:23 +0100

    Ajout d'une verification si le panier est vide

--- HeraWebsite/src/main/webapp/app/my-cart/my-cart.component.ts
+++ HeraWebsite/src/main/webapp/app/my-cart/my-cart.component.ts
@@ -4,13 +4,7 @@ import { HttpResponse } from '@angular/common/http';
 import { ProductService } from 'app/shared/service/product.service';
 import { IProduct } from 'app/shared/model/product.model';
 import { Order } from '../shared/model/order.model';
-
-/*
-  TO ADD:
-    Error message when the user is not connected and invite him to connect
-    Error message when there is no item products in the cart
-    ERRORS WITH BRANCHES
-*/
+import { LoginModalService } from 'app/core';
 
 @Component({
     selector: 'jhi-my-cart',
@@ -22,8 +16,14 @@ export class MyCartComponent implements OnInit {
     currentUser: IUser;
     cartProducts: IProduct[];
     totalCost: Number = 0;
+    confirmation: Boolean = false;
 
-    constructor(public principal: Principal, private productService: ProductService, private userService: UserService) {}
+    constructor(
+        public principal: Principal,
+        private productService: ProductService,
+        private userService: UserService,
+        private loginService: LoginModalService
+    ) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
@@ -33,7 +33,7 @@ export class MyCartComponent implements OnInit {
                 this.productService.queryBasket(this.currentUser).subscribe((cart: HttpResponse<IProduct[]>) => {
                     this.cartProducts = cart.body;
                     this.totalCost = this.getTotalCost();
-                    // console.log(this.cartProducts);
+                    this.confirmation = true;
                 });
             });
         });
@@ -50,7 +50,7 @@ export class MyCartComponent implements OnInit {
     }
 
     pay() {
-        let order = new Order();
+        const order = new Order();
         this.principal.identity().then(account => {
             this.accountConnected = account;
             this.userService.find(this.accountConnected.login).subscribe((res: HttpResponse<IUser>) => {
@@ -70,5 +70,17 @@ export class MyCartComponent implements OnInit {
             total += entry.quantity * entry.price;
         }
         return total;
+    }
+
+    logIn() {
+        this.loginService.open();
+    }
+
+    cartIsEmpty() {
+        let empty = true;
+        if (this.cartProducts.length > 0) {
+            empty = false;
+        }
+        return empty;
     }
 }
